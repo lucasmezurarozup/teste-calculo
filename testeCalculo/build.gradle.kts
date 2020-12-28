40620af339265089431f8a6d056fe31f3dfb18d1
Lucas Mezuraro <lucaspmezuraro@gmail.com>
2020-12-28 12:11:45 -0300

    alteracoes para incluir grpc

--- testeCalculo/build.gradle.kts
+++ testeCalculo/build.gradle.kts
@@ -1,13 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*;
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.10"
     id("org.jetbrains.kotlin.kapt") version "1.4.10"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("io.micronaut.application") version "1.2.0"
+    id("com.google.protobuf") version "0.8.13"
+    id("idea")
 }
 
 version = "0.1"
 group = "br.com"
+
+
+buildscript {
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.13")
+    }
+}
+
 
 val kotlinVersion=project.properties.get("kotlinVersion")
 repositories {
@@ -47,8 +60,37 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("io.micronaut.test:micronaut-test-junit5:2.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+    api("com.google.protobuf:protobuf-java-util:3.13.0")
+    implementation("io.grpc:grpc-all:1.33.1")
+    api("io.grpc:grpc-kotlin-stub:0.2.1")
+    implementation("io.grpc:protoc-gen-grpc-kotlin:0.1.5")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.grpc:grpc-protobuf:1.33.1")
+    implementation("io.grpc:grpc-stub:1.33.1")
+    implementation("io.grpc:grpc-netty:1.33.1")
+
+    implementation("io.micronaut.grpc:micronaut-grpc-server-runtime")
+    implementation("io.micronaut.grpc:micronaut-grpc-client-runtime")
 }
 
+/*sourceSets {
+    main {
+        java {
+            "build/generated/source/proto/main/grpc"
+            "build/generated/source/proto/main/grpckt"
+            "build/generated/source/proto/main/java"
+        }
+    }
+}*/
+
+idea {
+    module {
+        sourceDirs.plusAssign(file("${projectDir}/src/generated/main/java"))
+        sourceDirs.plusAssign(file("${projectDir}/src/generated/main/grpc"))
+        sourceDirs.plusAssign(file("${projectDir}/src/generated/main/grpckt"))
+    }
+}
 
 application {
     mainClass.set("br.com.ApplicationKt")
@@ -57,6 +99,15 @@ application {
 java {
     sourceCompatibility = JavaVersion.toVersion("11")
 }
+
+/*allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+        google()
+    }
+}*/
 
 tasks {
     compileKotlin {
@@ -70,6 +121,30 @@ tasks {
         }
     }
 
+}
 
+
+
+protobuf {
+    protoc{
+        artifact = "com.google.protobuf:protoc:3.10.1"
+    }
+
+    plugins {
+        id("grpc"){
+            artifact = "io.grpc:protoc-gen-grpc-java:1.33.1"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:0.1.5"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
 }
 
---
+++ testeCalculo/src/main/kotlin/br/com/server/IOFEndpoint.kt
@@ -0,0 +1,31 @@
+package br.com.server
+
+import br.com.calculo.Simulacao
+import br.com.calculo.iof.IOFServiceGrpc
+import br.com.calculo.iof.IOFServiceGrpcKt
+import br.com.calculo.iof.PropostaRequest
+import br.com.calculo.iof.SimulacaoResponse
+import br.com.proposta.Proposta
+import br.com.proposta.Situacao
+import br.com.proposta.TipoEmprestimo
+import io.grpc.stub.StreamObserver
+import java.math.BigDecimal
+import javax.inject.Singleton
+
+@Singleton
+class IOFEndpoint: IOFServiceGrpcKt.IOFServiceCoroutineImplBase() {
+
+
+
+    override suspend fun simular(request: PropostaRequest): SimulacaoResponse {
+
+        val proposta = Proposta(request.nome, request.meses, BigDecimal.valueOf(request.valor), Situacao.SOLICITADO, TipoEmprestimo.MICRO_CREDITO)
+
+        val simulacao = Simulacao(proposta)
+        System.out.println("Nome: "+request.nome);
+        System.out.println("Meses: "+request.meses);
+        System.out.println("Valor: "+request.valor);
+
+        return SimulacaoResponse.newBuilder().setMontanteAPagar(simulacao.calcula().getMontanteAPagar().toDouble()).build()
+    }
+}
